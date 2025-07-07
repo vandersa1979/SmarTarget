23ae9d50be11d4af6693d57faafbf73bd845f897
vandersa1979 <vsabreu@iportsolutions.com.br>
2025-07-06 23:19:17 -0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,49 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+
+// Firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyAmf4kImeUaJOcisVYGxhaboUWZon36o9A",
+  authDomain: "smartarget-web-bd.firebaseapp.com",
+  projectId: "smartarget-web-bd",
+  storageBucket: "smartarget-web-bd.firebasestorage.app",
+  messagingSenderId: "1045284868116",
+  appId: "1:1045284868116:web:d6a49d39dd0ca1bb0fd701",
+  measurementId: "G-3Q65NT8VV0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// CRUD functions
+export async function createItem(collectionName, data) {
+  const colRef = collection(db, collectionName);
+  const docRef = await addDoc(colRef, data);
+  return docRef.id;
+}
+
+export async function readItems(collectionName) {
+  const colRef = collection(db, collectionName);
+  const snapshot = await getDocs(colRef);
+  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+}
+
+export async function updateItem(collectionName, id, data) {
+  const docRef = doc(db, collectionName, id);
+  await updateDoc(docRef, data);
+}
+
+export async function deleteItem(collectionName, id) {
+  const docRef = doc(db, collectionName, id);
+  await deleteDoc(docRef);
+}
